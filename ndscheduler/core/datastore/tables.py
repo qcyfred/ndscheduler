c75a937c958becde20c0e60dac83fc06ea1dd8b5
qcyfred <qcy_1993@126.com>
2018-01-05 15:52:36 +0800

    删掉刚刚添加的表。暂时没有必要。

--- ndscheduler/core/datastore/tables.py
+++ ndscheduler/core/datastore/tables.py
@@ -45,39 +45,39 @@ AUDIT_LOGS = sqlalchemy.Table(
 
 
 
+# #
+# # 执行结果
+# # 按列存放
+# # 日期、任务id、任务名称、执行参数、返回值（TEXT）、执行状态
+# #
+# EXECUTION_RESULTS = sqlalchemy.Table(
+#     'execution_results', METADATA,
+#     sqlalchemy.Column('eid', sqlalchemy.Unicode(191, _warn_on_bytestring=False), primary_key=True),
+#     sqlalchemy.Column('hostname', sqlalchemy.Text, nullable=True),
+#     sqlalchemy.Column('pid', sqlalchemy.Integer, nullable=True),
+#     sqlalchemy.Column('state', sqlalchemy.Integer, nullable=False),
+#     sqlalchemy.Column('scheduled_time', sqlalchemy.DateTime(timezone=True), nullable=False,
+#                       default=utils.get_current_datetime),
+#     sqlalchemy.Column('updated_time', sqlalchemy.DateTime(timezone=True),
+#                       default=utils.get_current_datetime, onupdate=utils.get_current_datetime),
+#     sqlalchemy.Column('description', sqlalchemy.Text, nullable=True),
+#     sqlalchemy.Column('result', sqlalchemy.Text, nullable=True),
+#     sqlalchemy.Column('job_params', sqlalchemy.Text, nullable=True),
+#     sqlalchemy.Column('job_id', sqlalchemy.Text, nullable=False),
+#     sqlalchemy.Column('task_id', sqlalchemy.Text, nullable=True))
 #
-# 执行结果
-# 按列存放
-# 日期、任务id、任务名称、执行参数、返回值（TEXT）、执行状态
 #
-EXECUTION_RESULTS = sqlalchemy.Table(
-    'execution_results', METADATA,
-    sqlalchemy.Column('eid', sqlalchemy.Unicode(191, _warn_on_bytestring=False), primary_key=True),
-    sqlalchemy.Column('hostname', sqlalchemy.Text, nullable=True),
-    sqlalchemy.Column('pid', sqlalchemy.Integer, nullable=True),
-    sqlalchemy.Column('state', sqlalchemy.Integer, nullable=False),
-    sqlalchemy.Column('scheduled_time', sqlalchemy.DateTime(timezone=True), nullable=False,
-                      default=utils.get_current_datetime),
-    sqlalchemy.Column('updated_time', sqlalchemy.DateTime(timezone=True),
-                      default=utils.get_current_datetime, onupdate=utils.get_current_datetime),
-    sqlalchemy.Column('description', sqlalchemy.Text, nullable=True),
-    sqlalchemy.Column('result', sqlalchemy.Text, nullable=True),
-    sqlalchemy.Column('job_params', sqlalchemy.Text, nullable=True),
-    sqlalchemy.Column('job_id', sqlalchemy.Text, nullable=False),
-    sqlalchemy.Column('task_id', sqlalchemy.Text, nullable=True))
-
-
-# 执行时软件打印的LOG
-EXECUTIONS_LOGS = sqlalchemy.Table(
-    'execution_logs', METADATA,
-    sqlalchemy.Column('eid', sqlalchemy.Unicode(191, _warn_on_bytestring=False), primary_key=True),
-    sqlalchemy.Column('hostname', sqlalchemy.Text, nullable=True),
-    sqlalchemy.Column('pid', sqlalchemy.Integer, nullable=True),
-    sqlalchemy.Column('log_level', sqlalchemy.Text, nullable=False),
-    sqlalchemy.Column('log_time', sqlalchemy.DateTime(timezone=True), nullable=False,
-                      default=utils.get_current_datetime),
-    sqlalchemy.Column('job_name', sqlalchemy.Text, nullable=True),
-    sqlalchemy.Column('job_id', sqlalchemy.Text, nullable=False),
-    sqlalchemy.Column('task_id', sqlalchemy.Text, nullable=True),
-    sqlalchemy.Column('task_name', sqlalchemy.Text, nullable=True)
-)
+# # 执行时软件打印的LOG
+# EXECUTIONS_LOGS = sqlalchemy.Table(
+#     'execution_logs', METADATA,
+#     sqlalchemy.Column('eid', sqlalchemy.Unicode(191, _warn_on_bytestring=False), primary_key=True),
+#     sqlalchemy.Column('hostname', sqlalchemy.Text, nullable=True),
+#     sqlalchemy.Column('pid', sqlalchemy.Integer, nullable=True),
+#     sqlalchemy.Column('log_level', sqlalchemy.Text, nullable=False),
+#     sqlalchemy.Column('log_time', sqlalchemy.DateTime(timezone=True), nullable=False,
+#                       default=utils.get_current_datetime),
+#     sqlalchemy.Column('job_name', sqlalchemy.Text, nullable=True),
+#     sqlalchemy.Column('job_id', sqlalchemy.Text, nullable=False),
+#     sqlalchemy.Column('task_id', sqlalchemy.Text, nullable=True),
+#     sqlalchemy.Column('task_name', sqlalchemy.Text, nullable=True)
+# )
